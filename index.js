b255bb734996633be4e5b763db60998dcc8e4529
Vitaliy <loban.99@list.ru>
2018-09-19 17:37:33 +0300

    print all command line arguments

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-const name= process.argv[2];
-console.log(`Hi ${name}!`);
+ z="";
+process.argv.forEach(element => {
+    if(element!=process.argv[0]&&element!=process.argv[1])
+    {
+    z+=element.toString()+',';  
+    }
+    
+});
+console.log(z);
