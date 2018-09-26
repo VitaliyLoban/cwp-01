8b53ff3eb6dbc0f8833f32740b1dc1e35c9124b3
Vitaliy <loban.99@list.ru>
2018-09-26 18:45:08 +0300

    task 05

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-
 const fs=require("fs");
 const path=require("path")
 const dir_path=process.argv[2];
 const full_path=dir_path+'/'+path.basename(dir_path);
-
 fs.appendFile(path.resolve(dir_path + "/summary.js"), `const fs = require('fs');
+const path = require('path');
+let newDir = "${dir_path}" + path.sep;
 let files_1;
-function getFiles (dir,files_){
-  if(files_==null){
-    let files_1=" ";
-  }
-  else{
-    files_1=files_;
-  }
-  let files = fs.readdirSync(dir);
-  for (let i in files){
-      let name = dir + '/' + files[i];
-      if (fs.statSync(name).isDirectory()){
-          getFiles(name,files_1);
-      } else {
-          files_1+=name+"\\n";
-      }
-  }
-  return files_1;
-}
-console.log(getFiles(".",null))`, function(err) {
+fs.mkdir(newDir, function (err) {
+  (
+    function getFiles(dir, files_) {
+      fs.readdir(dir, function (err, files) {
+        for (let i in files) {
+          let name = dir + '/' + files[i];
+          fs.stat(name, (err, stats) => {
+            if (stats.isDirectory()) {
+              getFiles(name, files_);
+            } else {
+              let filename = path.basename(name);
+              let extname = path.extname(filename);
+              let contents = fs.readFile("E:/лабы 5 сем/node/cwp-01/config.json", "utf8", function (err, data) {
+                let copright = JSON.parse(data).copyright;
+                if (extname == ".txt") {
+                  fileContent = fs.readFile(name, "utf8", function (err, data) {
+                    fs.appendFile(newDir + '/' + filename, copright + "\\n" + data + "\\n", function (err) {
+                      if (err) throw err;
+                    });
+                    if (err) throw err;
+                  })
+                }
+                if (err) throw err;
+              })
+              console.log(name);
+            }
+          });
+        }
+      });
+
+    }
+  )(".", null);
+  if (err) throw err;
+});`, function(err) {
     if(err) throw err; 
     }); 
-// // console.log(full_path);
-// fs.writeFileSync(`${z+'/summary.js'}`,`var fs=require('fs');
-// fs.readdir(path, function(err, items) {
-//         console.log(items); 
-//         for (var i=0; i<items.length; i++) {
-//              console.log(items[i]);    }});`
-// // );
